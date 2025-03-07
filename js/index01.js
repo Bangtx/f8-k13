7420d33c9459f6cc54b1899823919f60837df1e3
bangtx <banhtran.hha@gmail.com>
2025-03-07 14:21:18 +0700

    dialog

---
+++ js/index01.js
@@ -0,0 +1,155 @@
+// day la comment
+/*
+* Bien - variable
+* luu tru tam thoi 1 gia tri nao do
+* luu tren ram
+*
+* js co x kieu du lieu
+* number
+* string
+* boolean bool
+* null
+* object
+* bigint
+* symbol
+* undefined
+*
+* Toan tu - operator
+*  + - * / luu thua (**) % ++ --
+*
+* phan biet a++ & ++a
+* */
+var fullName = 'vu xuan giang'
+var text = '<input value="duoc"/>'
+
+// bool true false
+/*
+* true: 1
+* false: 0
+*
+* phép hội (nhân / và)
+* phép hội chỉ đúng khi 2 số cùng đúng
+*
+   phep va (hoi / nhan)
+   x     y     x&y
+   1     1      1
+   1     0      0
+   0     1      0
+   0     0      0
+
+* phép tuyển (cộng / hoặc)
+   phep hoac (tuyen / cong)
+   x     y     x|y
+   1     1      1
+   1     0      1
+   0     1      1
+   0     0      0
+* */
+
+var a = "30"
+var b = Number(a)
+
+/*
+* dieu kien (condition)
+* > < >= <= == === != !==
+* */
+// console.log(3 < 2 || 4 < 5 && 5 < 6)
+
+// bai toan: cho 1 so cho trc a = x, kiem tra a la so chan hay le
+
+/*
+*
+   khai bao a
+     │
+     ▼     = 0
+   a % 2 ────────►  chan
+     │
+     │= 1
+     │
+     ▼
+    le
+* */
+// console.log(3 !== 2)
+//
+// var a = 11
+// if (a % 2 === 0) {
+//     console.log('a la so chan')
+// }
+// else {
+//     console.log('a la so le')
+// }
+
+
+// cho phuong trinh a * x^2 + bx + c = 0
+// cho trc a, b, c
+// tim x
+var a = 5, b = 6, c = 1
+
+/*
+*
+             khoi tao a, b, c
+                   │
+         <0        ▼             =0
+ vo no ◄──── delta = b*b - 4*a*c ────► co nghiem kep
+                   │                          -b
+                   │ >0                   x = ──
+                   ▼                          2a
+             co 2 nghiem phan biet
+
+                   -b - can(delta)
+             x1 = ─────────────────
+                         2a
+
+                   -b + can(delta)
+             x1 = ─────────────────
+                         2a
+* */
+
+/*
+*  luong
+*
+* */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
